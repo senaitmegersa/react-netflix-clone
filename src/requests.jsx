85426ef7289fb8c41ef9541b6b2a143f3f405084
Seni0215 <97817727+Seni0215@users.noreply.github.com>
2022-04-03 12:22:17 -0400

    my first commit

---
+++ src/requests.jsx
@@ -0,0 +1,33 @@
+const API_KEY = "1710c743f12fa55176fd5d9f932412d4";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//myAPI Key
+// 1710c743f12fa55176fd5d9f932412d4
+
+//Example API Request
+///https://api.themoviedb.org/3/movie/550?api_key=1710c743f12fa55176fd5d9f932412d4
